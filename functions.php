3533fcb17d0792ea2187f26b20b02c57c169ecde
saitoomituru <76672157+saitoomituru@users.noreply.github.com>
2023-08-13 01:32:26 +0900

    ロゴジェネレータ管理画面UI実装

--- functions.php
+++ functions.php
@@ -1,5 +1,4 @@
 <?php
-
 //  親テーマのスタイルをロードする
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
@@ -14,4 +13,51 @@ function arrow_mime_types($mimes) {
   return $mimes;
 }
 add_filter('upload_mimes', 'arrow_mime_types');
+
+//オリジナルカスタマイザー
+
+/**
+ * カスタマイザーの設定
+ */
+
+use Arkhe_Theme\Customizer;
+add_action( 'customize_register', __NAMESPACE__ . '\customizer_setup', 21 );
+function customizer_setup( $wp_customize ) {
+  Customizer::big_title( 'arkhe_section_global', 'breadcrumb_list',
+	array(
+		'label' => 'SVGロゴカラーエフェクタ',
+	)
+  );
+  Customizer::add(
+    'arkhe_section_global',
+    'default_Logo_color',
+    array(
+      'type' => 'color',
+      'label' => 'ロゴカラー',
+      'description' => "ロゴのカラーを置き換えることができます。他の設定で上がかれない限りこのカラーが反映します。SVG以外のロゴの場合は反映しません。",
+    ),
+    
+  );
+  Customizer::add(
+    'arkhe_section_global',
+    'default_Logo_color_b',
+    array(
+      'type' => 'checkbox',
+      'label' => 'ロゴカラー変更する',
+      'description' => "このチェックをONにするとロゴの色をリライトします",
+      'choices' => 'ロゴの色を書き換える',
+      'default' => '',
+    ),
+    
+  );
+}
+
+add_filter('ark_get__head_logo_img','is_logo');
+function is_logo($imgcode){
+if(isset( $args['default_Logo_color_b'] )){
+  $imgcode = str_replace('<img','<use',$imgcode);
+}
+return $imgcode;
+}
+
 ?>
